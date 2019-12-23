938bbcb431550f2da968af91465e13d9fb21c3b6
coolAlias <BASbliznet@gmail.com>
2019-12-22 16:04:52 -0800

    Updated to v1.5.1

--- src/main/java/swordskillsapi/SwordSkillsApi.java
+++ src/main/java/swordskillsapi/SwordSkillsApi.java
@@ -20,7 +20,7 @@ public class SwordSkillsApi
 {
 	public static final String ID = "swordskillsapi";
 	public static final String NAME = "Sword Skills API";
-	public static final String VERSION = "1.11.2-1.1.0";
+	public static final String VERSION = "1.11.2-1.5.1";
 	public static final String VERSION_LIST = "https://raw.githubusercontent.com/coolAlias/SwordSkillsApi/master/src/main/resources/versionlist.json";
 
 	@Mod.Instance(ID)
